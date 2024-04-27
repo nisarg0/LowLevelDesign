187e6208e55d64133973e6c0a5ab79066ef751c1
nisarg <nisarggogate212@gmail.com>
2024-04-27 23:51:01 +0530

    Added Decorator Design Pattern

---
+++ src/main/java/DesignPatterns/decoratorDesignPattern/PizzaMaker.java
@@ -0,0 +1,94 @@
+package DesignPatterns.decoratorDesignPattern;
+
+abstract class BasePizza {
+    public abstract int cost();
+}
+
+// Follows both IS-A and HAS-A relationships
+
+abstract class ToppingDecorator extends BasePizza {
+    BasePizza basePizza;
+    public abstract int cost();
+}
+
+class VeggieDelightPizza extends BasePizza {
+    @Override
+    public int cost() {
+        return 160;
+    }
+}
+
+class FarmhousePizza extends BasePizza {
+    @Override
+    public int cost() {
+        return 125;
+    }
+}
+
+class MargaritaPizza extends BasePizza {
+    @Override
+    public int cost() {
+        return 100;
+    }
+}
+
+class CheeseTopping extends ToppingDecorator {
+    BasePizza basePizza;
+
+    CheeseTopping(BasePizza pizza) {
+        basePizza = pizza;
+    }
+
+    @Override
+    public int cost() {
+        return basePizza.cost() + 50;
+    }
+}
+
+class OliveTopping extends ToppingDecorator {
+    BasePizza basePizza;
+
+    OliveTopping(BasePizza pizza) {
+        basePizza = pizza;
+    }
+
+    @Override
+    public int cost() {
+        return basePizza.cost() + 20;
+    }
+}
+
+class PannerTopping extends ToppingDecorator {
+    BasePizza basePizza;
+
+    PannerTopping(BasePizza pizza) {
+        basePizza = pizza;
+    }
+
+    @Override
+    public int cost() {
+        return basePizza.cost() + 100;
+    }
+}
+
+
+
+
+public class PizzaMaker {
+    public static void main(String[] args) {
+        BasePizza veggieDelightPizza = new VeggieDelightPizza();   // Plain veggie delight pizza
+        veggieDelightPizza = new CheeseTopping(veggieDelightPizza); // veggieDelight is assigned 1 topping
+        veggieDelightPizza = new OliveTopping(veggieDelightPizza); // veggieDelight is reassigned value with two toppings
+        System.out.println("Cost of Veggie Delight + Cheese & Olive toppings is " + veggieDelightPizza.cost());
+
+        BasePizza farmhousePizza = new FarmhousePizza();
+        farmhousePizza = new CheeseTopping(farmhousePizza);
+        farmhousePizza = new OliveTopping(farmhousePizza);
+        System.out.println("Cost of farmhouse with Cheese and Olive toppings is " + farmhousePizza.cost());
+
+        BasePizza margaritaPizza = new MargaritaPizza();
+        margaritaPizza = new PannerTopping(margaritaPizza);
+        System.out.println("Cost of MargaritaPizza with Panner topping is " + margaritaPizza.cost());
+        
+    }
+}
---
+++ src/main/java/DesignPatterns/decoratorDesignPattern/README.md
@@ -0,0 +1,25 @@
+### Decorator Design Pattern
+- It helps us add new features to existing object without altering its structure.
+- It provides a wrapper to existing class.
+
+Note:
+- Decorator design pattern is useful in providing runtime modification abilities and hence more flexible. Its easy to maintain and extend when the amount of choices are more.
+- Decorator pattern is used a lot in Java IO classes, like FileReader, BufferedReader, etc.
+
+#### This pattern helps us from class explosion.
+
+![img.png](../Assets/decoratorDesignPattern.png)
+Cruz of this pattern is decorator is a base class and has a base class. Hence it becomes extensible.
+
+More toppings can easily be added.
+
+Output:
+```agsl
+Cost of Veggie Delight + Cheese & Olive toppings is 230
+Cost of farmhouse with Cheese and Olive toppings is 195
+Cost of MargaritaPizza with Panner topping is 200
+```
+
+---
+Ref:
+- https://www.youtube.com/watch?v=w6a9MXUwcfY
